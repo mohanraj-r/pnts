f139f75ade3be921db758b15d10fc37682c0bfd2
Mohan Raj Rajamanickam <mohanraj.cit@gmail.com>
2013-06-23 19:37:15 -0400

    fixed some indendation issues with the program. added a basic test script template

---
+++ test_vending_machine.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 23, 2013
+
+@author: mohanr
+'''
+import unittest
+
+from vending_machine import vend_pop
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def test(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
