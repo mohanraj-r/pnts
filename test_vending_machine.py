5aa728a1d88f35d0201f2f13e124dbb6980c51f8
Mohan Raj Rajamanickam <mohanraj.cit@gmail.com>
2013-06-24 14:24:22 -0400

    Generated 2 tests dynamically from data

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -5,12 +5,41 @@ Created on Jun 23, 2013
 '''
 import random
 import string
-import unittest
+
+import nose
 
 from vending_machine import vend_pop, POP_VENDING_MACHINE
 
 
-class TestVendingMachine(unittest.TestCase):
+def _get_random_string(chars=string.letters, length=10):
+    ''' Generate a random string with given chars and of given length. '''
+    chars_len = len(chars) - 1
+    random_string = ''.join([chars[random.randint(0, chars_len)] 
+                             for _ in range(length)])
+    return random_string
+
+
+def _get_valid_random_pop(POP=POP_VENDING_MACHINE):
+    ''' Get the name of a pop randomly from available pops. '''
+    return POP.keys()[random.randint(0, len(POP)-1)]
+
+
+def _get_cost_of_pop(pop):
+    ''' Get cost of a given pop.''' 
+    return POP_VENDING_MACHINE[pop]
+
+
+def _invalid_input(test_data):
+#     invalid_input.description = None
+    try:
+        vend_pop(**test_data['inputs'])
+    except test_data['error'] as exception:
+        pass
+    else:
+        assert test_data['assert_msg']            
+
+
+class TestVendingMachine(object):
     '''
     Tests for the vending machine.
     
@@ -20,26 +49,38 @@ class TestVendingMachine(unittest.TestCase):
         - or unittest (ref: http://stackoverflow.com/questions/32899/how-to-generate-dynamic-parametrized-unit-tests-in-python)
     '''
     
-    @staticmethod
-    def _generate_random_string(chars=string.letters, length=10):
-        ''' Generate a random string with given chars and of given length. '''
-        chars_len = len(chars) - 1
-        random_string = ''.join([chars[random.randint(0, chars_len)] 
-                                 for _ in range(length)])
-        return random_string
+    def __init__(self):
+        self.invalid_input_tests = {
+                                    'null_input': {
+                                                    'test_name': 'Test with Null inputs.',
+                                                    'inputs': {
+                                                                'pop_selection': None,
+                                                                'money_amount_paid': None,
+                                                                },
+                                                    'error': TypeError,
+                                                    'error_msg': None,
+                                                    'assert_msg': 'Vending pop with null inputs did not give expected error.',
+                                                    },
+                                    
+                                    'non_existing_pop_selection':{
+                                                                  'test_name': 'Test by asking for a non-existing pop selection.',
+                                                                  'inputs': {
+                                                                             'pop_selection': _get_random_string(),
+                                                                             'money_amount_paid': 1,
+                                                                             },
+                                                                  'error': ValueError,
+                                                                  'error_msg': None,
+                                                                  'assert_msg': 'Vending pop with a random string did not give expected error.'
+                                                                  }
+                                    }
+        
 
+    def test_invalid_inputs(self):
+        ''' Test generator to test different combinations of invalid inputs. '''
+        for test_name, test_data in self.invalid_input_tests.items():
+            _invalid_input.description = test_data.get('test_name', test_name)
+            yield _invalid_input, test_data
 
-    @staticmethod
-    def _get_valid_random_pop(POP=POP_VENDING_MACHINE):
-        ''' Get the name of a pop randomly from available pops. '''
-        return POP.keys()[random.randint(0, len(POP)-1)]
-
-
-    @staticmethod
-    def _get_cost_of_pop(pop):
-        ''' Get cost of a given pop.''' 
-        return POP_VENDING_MACHINE[pop]
-    
 
     def test_invalid_input_no_input(self):
         ''' Test without giving any input. The program takes non-optional arguments and hence should fail. '''
@@ -50,31 +91,11 @@ class TestVendingMachine(unittest.TestCase):
         else:
             assert 'Vending pop without input did not give expected error.'
    
-    
-    def test_invalid_input_null_inputs(self):
-        ''' Test with Null inputs.  '''
-        try:
-            vend_pop(pop_selection=None, money_amount_paid=None)
-        except TypeError:
-            pass
-        else:
-            assert 'Vending pop with null inputs did not give expected error.'
-
-
-    def test_non_existing_pop_selection(self):
-        ''' Test by asking for a non-existing pop selection. '''
-        try:
-            vend_pop(pop_selection=self._generate_random_string(), money_amount_paid=10)
-        except ValueError:
-            pass
-        else:
-            assert 'Vending pop with a random string did not give expected error.'
-
 
     def test_invalid_input_zero_amount_paid(self):
         ''' Test with zero amount paid. '''
         try:
-            vend_pop(pop_selection=self._get_valid_random_pop(), money_amount_paid=0)
+            vend_pop(pop_selection=_get_valid_random_pop(), money_amount_paid=0)
         except ValueError:
             pass
         else:
@@ -83,8 +104,8 @@ class TestVendingMachine(unittest.TestCase):
 
     def test_insufficient_amount(self):
         ''' Test by trying to get a pop with insufficient amount. '''
-        pop = self._get_valid_random_pop()
-        cost = self._get_cost_of_pop(pop)
+        pop = _get_valid_random_pop()
+        cost = _get_cost_of_pop(pop)
         test_amount = cost - 1 
         try:
             vend_pop(pop_selection=pop, money_amount_paid=test_amount)
@@ -96,8 +117,8 @@ class TestVendingMachine(unittest.TestCase):
 
     def test_change_returned(self):
         ''' Test if correct change is returned after a valid vending operation. '''
-        pop = self._get_valid_random_pop()
-        cost = self._get_cost_of_pop(pop)
+        pop = _get_valid_random_pop()
+        cost = _get_cost_of_pop(pop)
         test_amount = cost + 1
         expected_change = test_amount - cost
         change_returned = vend_pop(pop_selection=pop, money_amount_paid=test_amount)
@@ -105,5 +126,4 @@ class TestVendingMachine(unittest.TestCase):
                 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+    nose.main()
