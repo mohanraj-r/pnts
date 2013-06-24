f25a0b8a02b2aa24152a16c11617500685e93c2f
Mohan Raj Rajamanickam <mohanraj.cit@gmail.com>
2013-06-23 20:03:00 -0400

    added 3 basic tests to check for invalid input

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -3,12 +3,18 @@ Created on Jun 23, 2013
 
 @author: mohanr
 '''
+import random
+import string
 import unittest
 
 from vending_machine import vend_pop
 
-class Test(unittest.TestCase):
 
+class Test(unittest.TestCase):
+    
+    def _generate_random_string(self, chars=string.letters, length=10):
+        random_string = ''.join([chars[random.randint(1, len(chars))] for _ in range(length)])
+        return random_string
 
     def setUp(self):
         pass
@@ -18,8 +24,34 @@ class Test(unittest.TestCase):
         pass
 
 
-    def test(self):
-        pass
+    def test_invalid_input_no_input(self):
+        ''' Test without giving any input. The program takes non-optional arguments and hence should fail. '''
+        try:
+            vend_pop()
+        except TypeError:
+            pass
+        else:
+            assert 'Vending pop without input did not yield expected error.'
+   
+    
+    def test_invalid_input_null_inputs(self):
+        ''' Test with Null inputs.  '''
+        try:
+            vend_pop(pop_selection=None, money_amount_paid=None)
+        except TypeError:
+            pass
+        else:
+            assert 'Vending pop with null inputs did not yield expected error.'
+
+
+    def test_non_existing_pop_selection(self):
+        ''' Test by asking for a non-existing pop selection. '''
+        try:
+            vend_pop(pop_selection=self._generate_random_string(), money_amount_paid=10)
+        except ValueError:
+            pass
+        else:
+            assert 'Vending pop with a random string did not yield expected error.'
 
 
 if __name__ == "__main__":
